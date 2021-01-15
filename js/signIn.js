dff28908911c935712b9fa3dc406790f0888cc76
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-01-15 18:28:44 +0600

    Add files via upload

--- js/signIn.js
+++ js/signIn.js
@@ -6,13 +6,13 @@ let userPassword = document.getElementById('UserPassword')
 function Redirect() {
 
     if (userName.value == 'test' && userPassword.value == '123') {
-        window.location = "sites/logInto.html";
+        window.location = "../sites/logInto.html";
     }
     else if (userName.value == '' && userPassword.value == '') {
         alert("Please Enter Username and Password")
     }
     else if (userName.value == 'admin' && userPassword.value == 'admin123') {
-        window.location = "sites/adminPanel.html";
+        window.location = "../sites/adminPanel.html";
     }
 
     else {
