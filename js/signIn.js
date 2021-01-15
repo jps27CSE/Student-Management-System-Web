4fc12c08fc4c40ced4d4aa5017211c1016ea7ad0
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-01-15 14:19:58 +0600

    Add files via upload

---
+++ js/signIn.js
@@ -0,0 +1,22 @@
+let userName = document.getElementById('UserName')
+
+let userPassword = document.getElementById('UserPassword')
+
+
+function Redirect() {
+
+    if (userName.value == 'test' && userPassword.value == '123') {
+        window.location = "sites/logInto.html";
+    }
+    else if (userName.value == '' && userPassword.value == '') {
+        alert("Please Enter Username and Password")
+    }
+    else if (userName.value == 'admin' && userPassword.value == 'admin123') {
+        window.location = "sites/adminPanel.html";
+    }
+
+    else {
+        alert('Username or Password Incorrect')
+    }
+
+}
